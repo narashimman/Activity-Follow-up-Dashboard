17ad08549033c60b3d3837802429e087e35fdf99
narashimman <34731067+narashimman@users.noreply.github.com>
2017-12-21 09:29:56 +0530

    Initial commit

---
+++ reports.js
@@ -0,0 +1,15 @@
+var reportIds = {
+  "1117": {"name" : "Summary"},
+  "26": {"name" : "Activity Follow-up"},
+  "312": {"name" : "Exploration with cascading input controls"},
+  "1832": {"name" : "Exploration with element linking"},
+  "35": {"name" : "References"}
+};
+
+var order = {
+ "0": "1117",
+ "1": "26",
+ "2": "312",
+ "3": "1832",
+ "4": "35"
+};
